92591f1a962e2f43d3ba0f080883520c9d504cdb
Shuredin <70452709+Shuredin@users.noreply.github.com>
2020-08-29 16:52:28 -0500

    Create Customer.h

---
+++ Customer.h
@@ -0,0 +1,22 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <iostream>
+
+using namespace std;
+
+class Customer {
+
+public:
+	Customer();
+	void toString();
+
+private:
+	string name;
+  int age;
+  string address;
+  string phone;
+  string email;
+};
+
+#endif // !CUSTOMER_H
